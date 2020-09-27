6dfa825b3215a0635d439611d103e1f5c6a1df15
JimPonce5 <71978894+JimPonce5@users.noreply.github.com>
2020-09-27 15:43:26 -0600

    Update LatinSquareDriver.java

--- prg01_latin_squares/src/LatinSquareDriver.java
+++ prg01_latin_squares/src/LatinSquareDriver.java
@@ -2,7 +2,7 @@
  * CS1050 - Computer Science I - Fall 2020
  * Instructor: Thyago Mota
  * Description: Prg01 - LatinSquare
- * Student(s) Name(s):
+ * Student(s) Name(s):Jim Ponce
  */
 
 import java.util.Scanner;
@@ -26,19 +26,43 @@ public class LatinSquareDriver {
         // the number entered  must be in [MIN_ORDER..MAX_ORDER];
         // the program should continuously ask the user for the
         // order of the square, until a valid number is entered.
-
-
+        Scanner sc =new Scanner(System.in);
+        boolean flag=false; int order = 0;int n=0;
+        boolean flag1=false;
+        while(flag==false){
+            System.out.println("Enter your first number in order: ");
+            order = sc.nextInt():
+            if(order>=MIN_ORDER && order<=MAX_ORDER)
+                flag= true;
         // TODO: ask the user for the first number of the square;
         // the number entered must be in [1..order];
         // the program should continuously ask the user for the
         // first number of the square, until a valid number is entered.
-
-
+            while(flag1=false){
+                System.out.println("Enter your first number: ");
+                n = sc.nextInt();
+                if(n>=1 && n <=order)
+                    flag1=true;
+            }
+            int m=n;
+            for(int i=0;i<order;j++){
+                System.out.print(m+" ");
+                m++;
+                if (m>order)
+                    m=1;
+            }
+            System.out.println();
+            if(n>=order){
+                m=1;
+                n=1;
+            }
+            else{
+                m=n+1;
+                n++;
+            }
         // TODO: generate the latin square;
         // after you run your code, the output must be saved in a file named latin_square_A_B.txt,
         // where A should be replaced by the order and B by the first number; for example,
         // if order is 5 and the first number is 2, the file should be named latin_square_5_2.txt
-
-
     }
 }
